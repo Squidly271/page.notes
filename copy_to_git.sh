3370a797e2df0cde6339793700319e221dba7967
Squidly271 <unraidsquid@gmail.com>
2021-04-03 20:21:56 -0400

    Try A

--- copy_to_git.sh
+++ copy_to_git.sh
@@ -3,6 +3,6 @@
 mkdir -p "/tmp/GitHub/page.notes/source/page.notes/usr/local/emhttp/plugins/page.notes/"
 
 cp /usr/local/emhttp/plugins/page.notes/* /tmp/GitHub/page.notes/source/page.notes/usr/local/emhttp/plugins/page.notes -R -v -p
-cd /tmp/GitHub/page.notes/source/page.notes/usr/local/emhttp/plugins/page.notes
+
 
 
